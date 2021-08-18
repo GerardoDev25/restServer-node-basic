ae2e8116e6bd06cdb90a210a24de249c3105e4aa
GerardoDev25 <GerardoDev25@gmail.com>
2021-08-18 17:09:10 -0400

    7. Login de usuario

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,53 @@
 const { response, request } = require("express");
+const bcryptjs = require("bcryptjs");
 
-const login = (req = request, res = response) => {
-   res.json({
-      mshg: "login ok",
-   });
+const UserModel = require("../models/user");
+
+const login = async (req = request, res = response) => {
+   const { email, password } = req.body;
+
+   try {
+      // * veriry if the eamil exist
+      const user = await UserModel.findOne({ email });
+      if (!user) {
+         return res
+            .status(400)
+            .json({
+               msg: "user or password isn't correct ¿ email",
+            });
+      }
+
+      // * veriry if the user is active
+      if (!user.state) {
+         return res.status(400).json({
+            msg: "user or password isn't correct ¿ state",
+         });
+      }
+
+      // * veriry the password
+      const validPassword = bcryptjs.compareSync(
+         password,
+         user.password
+      );
+      if (!validPassword) {
+         return res
+            .status(400)
+            .json({
+               msg: "user or password isn't correct ¿ password",
+            });
+      }
+
+      // * generate jwt
+
+      res.json({
+         mshg: "login ok",
+      });
+   } catch (error) {
+      console.error(error);
+      res.status(500).json({
+         msg: "something went wrong, please talk with the admin",
+      });
+   }
 };
 
 module.exports = { login };
