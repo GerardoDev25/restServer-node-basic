39cfabf4715dafde5c44e76185efad9a590108c4
GerardoDev25 <GerardoDev25@gmail.com>
2021-08-26 19:33:29 -0400

    13. Búsquedas en base de datos

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,10 +1,52 @@
 const { response, request } = require("express");
+const { ObjectId } = require("mongoose").Types;
+
+const {
+   User: UserModel,
+   Category: CategoryModel,
+   Product: ProductModel,
+} = require("../models");
+
+// ! ----------------------------------------------------
+
+const collections = ["users", "categories", "products", "roles"];
+
+const searchUsers = async (term, res = response) => {
+   const isMondoId = ObjectId.isValid(term);
+
+   if (isMondoId) {
+      const user = await UserModel.findById(term);
+      res.json({ results: user ? [user] : [] });
+   }
+};
 
 // ? Get serach products and categories
 const search = (req = request, res = response) => {
    const { collection, term } = req.params;
 
-   res.json({ collection, term });
+   if (!collections.includes(collection))
+      return res.status(400).json({
+         msg: `the allowed collections are ${collections}`,
+      });
+
+   switch (collection) {
+      case collections[0]:
+         searchUsers(term, res);
+         break;
+
+      case collections[1]:
+         break;
+
+      case collections[2]:
+         break;
+
+      default:
+         res.status(500).json({
+            msg: "I forgot to do this search",
+         });
+
+         break;
+   }
 };
 
 module.exports = { search };
