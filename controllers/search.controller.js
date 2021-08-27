bb77478902f798b1f161ad7be100cbd0c172d9fa
GerardoDev25 <GerardoDev25@gmail.com>
2021-08-26 20:27:18 -0400

    15. Buscar en otras colecciones

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -11,25 +11,88 @@ const {
 
 const collections = ["users", "categories", "products", "roles"];
 
-// ? search user by id
+// ? search user
 const searchUsers = async (term, res = response) => {
    const isMondoId = ObjectId.isValid(term);
 
    // * search by id
    if (isMondoId) {
       const user = await UserModel.findById(term);
-      return res.json({ results: user ? [user] : [] });
+      return res
+         .status(200)
+         .json({ results: user ? [user] : [] });
    }
 
    // * serach by name or email
    const regex = new RegExp(term, "i");
-   const users = await UserModel.find({
+   const conditions = {
       $or: [{ name: regex }, { email: regex }],
       $and: [{ state: true }],
-   });
+   };
 
-   // * send result
-   res.json({ results: users });
+   const [total, users] = await Promise.all([
+      UserModel.count(conditions),
+      UserModel.find(conditions),
+   ]);
+
+   // * send results
+   res.status(200).json({ total, results: users });
+};
+
+// ? search categories
+const searchCategories = async (term, res = response) => {
+   // * search by id
+   if (ObjectId.isValid(term)) {
+      const users = await CategoryModel.findById(term);
+      return res
+         .status(200)
+         .json({ results: users ? [users] : [] });
+   }
+
+   // * search by name
+   const regex = new RegExp(term, "i");
+   const conditions = {
+      $or: [{ name: regex }],
+      $and: [{ state: true }],
+   };
+   const [total, categories] = await Promise.all([
+      CategoryModel.count(conditions),
+      CategoryModel.find(conditions),
+   ]);
+
+   // * send results
+   res.status(200).json({ total, results: categories });
+};
+
+// ? search products
+const searchProducts = async (term, res = response) => {
+   // * serach by id
+   if (ObjectId.isValid(term)) {
+      const products = await ProductModel.findById(
+         term
+      ).populate("categorie", "name");
+      return res.status(200).json({
+         results: products ? [products] : [],
+      });
+   }
+
+   // * search by name and description
+   const regex = new RegExp(term, "i");
+   const conditions = {
+      $or: [{ name: regex }, { description: regex }],
+      $and: [{ state: true }],
+   };
+
+   const [total, products] = await Promise.all([
+      ProductModel.count(conditions),
+      ProductModel.find(conditions).populate(
+         "categorie",
+         "name"
+      ),
+   ]);
+
+   // * send resuts
+   res.status(200).json({ total, results: products });
 };
 
 // ? Get serach products and categories
@@ -47,9 +110,11 @@ const search = (req = request, res = response) => {
          break;
 
       case collections[1]:
+         searchCategories(term, res);
          break;
 
       case collections[2]:
+         searchProducts(term, res);
          break;
 
       default:
