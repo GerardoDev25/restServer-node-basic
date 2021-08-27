9493f6b4f757e5498a0efd66b2d1f2b3fdefaedd
GerardoDev25 <GerardoDev25@gmail.com>
2021-08-27 19:13:20 -0400

    5. Validar la extensión

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const { request, response } = require("express");
 
+// ? POST upload file
 const loadFile = async (req = request, res = response) => {
    //
 
@@ -18,21 +19,34 @@ const loadFile = async (req = request, res = response) => {
    // * get file
    const { file } = req.files;
 
-   // * create path of the file
-   const uploadPath = path.join(
-      __dirname,
-      "../uploads/",
-      file.name
-   );
+   // * get extencion
+   const cutName = file.name.split(".");
+   const extension = cutName[cutName.length - 1];
 
-   // * uploading the file
-   file.mv(uploadPath, (err) => {
-      if (err) return res.status(500).json({ err });
+   // * velidate extension of file
+   const extensionsValid = ["png", "jpg", "jpeg", "gif"];
+   if (!extensionsValid.includes(extension))
+      return res
+         .status(400)
+         .json({ msg: `extension ${extension} invalid` });
 
-      res.status(200).json({
-         msg: "File uploaded to " + uploadPath,
-      });
-   });
+   res.json({ extencion: extension });
+
+   //    // * create path of the file
+   //    const uploadPath = path.join(
+   //       __dirname,
+   //       "../uploads/",
+   //       file.name
+   //    );
+
+   //    // * uploading the file
+   //    file.mv(uploadPath, (err) => {
+   //       if (err) return res.status(500).json({ err });
+
+   //       res.status(200).json({
+   //          msg: "File uploaded to " + uploadPath,
+   //       });
+   //    });
 };
 
 module.exports = { loadFile };
