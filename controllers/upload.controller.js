76349a013dacc6dcf1e2fccef04d60bb639d2db8
GerardoDev25 <GerardoDev25@gmail.com>
2021-08-27 21:35:35 -0400

    7. Helper - SubirArchivo

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,52 +1,23 @@
-const path = require("path");
-const { v4: uuidv4 } = require("uuid");
 const { request, response } = require("express");
+const { uploadFile } = require("../helpers");
 
-// ? POST upload file
+// ? POST load file
 const loadFile = async (req = request, res = response) => {
    //
 
    // * if come a file property in the request
-   if (!req.files || Object.keys(req.files).length === 0)
+   if (
+      !req.files ||
+      Object.keys(req.files).length === 0 ||
+      !req.files.file
+   )
       return res
          .status(400)
          .json({ msg: "No files were uploaded." });
 
-   if (!req.files.file)
-      return res
-         .status(400)
-         .json({ msg: "No files were uploaded." });
+   const name = await uploadFile(req.files);
 
-   // * get file
-   const { file } = req.files;
-
-   // * get extencion
-   const cutName = file.name.split(".");
-   const extension = cutName[cutName.length - 1];
-
-   // * velidate extension of file
-   const extensionsValid = ["png", "jpg", "jpeg", "gif"];
-   if (!extensionsValid.includes(extension))
-      return res
-         .status(400)
-         .json({ msg: `extension ${extension} invalid` });
-
-   // * create path of the file
-   const nameTemp = uuidv4() + "." + extension;
-   const uploadPath = path.join(
-      __dirname,
-      "../uploads/",
-      nameTemp
-   );
-
-   // * uploading the file
-   file.mv(uploadPath, (err) => {
-      if (err) return res.status(500).json({ err });
-
-      res.status(200).json({
-         msg: "File uploaded to " + uploadPath,
-      });
-   });
+   res.json({ name });
 };
 
 module.exports = { loadFile };
